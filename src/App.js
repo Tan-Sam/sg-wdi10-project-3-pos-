4464e1591178e203c10f137a6a48884a41daabb5
sam <Tan-Sam@users.noreply.github.com>
2017-06-30 11:14:08 +0800

    updated pages wordings

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>SG-WDI-10-Project-3 test</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Test git between Ubunutu & MAC works.
         </p>
       </div>
     );
